924b21c686f7145e6ee46066772792cca930645c
rporrini <r.porrini@gmail.com>
2016-10-23 18:16:04 +0200

    refactoring

--- src/sockets.js
+++ src/sockets.js
@@ -2,8 +2,10 @@ const express = require('express');
 const application = express();
 const sockets = require('express-ws')(application);
 
-const startOn = (port) => {
-  application.ws('/alive', function(ws, req) {});
+const startOn = port => {
+  application.ws('/alive', function(ws, req) {
+    ws.send('OK');
+  });
   application.listen(port);
 }
 
--- test/alive.test.js
+++ test/alive.test.js
@@ -1,7 +1,19 @@
 describe('alive socket', function() {
+
   it('should listen for incoming connections', function(done) {
-    sockets.connect('/alive').on('open', function open() {
+
+    sockets.connect('/alive').on('open', function() {
       done();
     });
+
+  });
+
+  it('should answer with an OK message', function(done) {
+
+    sockets.connect('/alive').on('message', function(data) {
+      expect(data).to.be.equal('OK');
+      done();
+    });
+
   });
 });
