c9f27c36cde9604974341b660b8068990f35d466
rporrini <r.porrini@gmail.com>
2016-10-24 00:31:00 +0200

    bella pagina di test

--- status/app.js
+++ status/app.js
@@ -2,8 +2,9 @@ angular.module('status', ['angular-websocket'])
        .controller('PostsController', function($websocket, $interval, $location){
          var posts = this;
          posts.status = {pending:true};
+         const endpoint = 'ws://' + $location.host() + ':' + $location.port();
          $interval(() => {
-            var dataStream = $websocket('ws://' + $location.host() + ':' + $location.port() + '/alive');
+            var dataStream = $websocket(endpoint + '/alive');
             dataStream.onMessage(function(message) {
               posts.status = {alive:true};
             });
@@ -11,4 +12,15 @@ angular.module('status', ['angular-websocket'])
               posts.status = {ko:true};
             });
          }, 5000);
+
+         posts.board = '';
+         const postSocket = $websocket(endpoint + '/post')
+         postSocket.onMessage(function(message){
+           posts.board += message.data + '\n';
+         });
+         posts.send = message => {
+           postSocket.send(message);
+           posts.board += message + '\n';
+           posts.message = '';
+         };
        });
