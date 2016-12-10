b89a7bf08430772c14ee785eae196482c0ac3740
rporrini <r.porrini@gmail.com>
2016-12-10 14:12:45 +0100

    exaustive test

--- test/performance/load.test.js
+++ test/performance/load.test.js
@@ -1,55 +1,60 @@
 const promise = require('bluebird');
 const sinon = require('sinon');
 
-const timeoutAfterSixtySeconds = onWhat => {
+load(10, 100);
+load(100, 1000);
+load(1000, 1000);
+load(2000, 2000);
+
+function timeoutAfterSixtySeconds(onWhat) {
 	onWhat.timeout(60000);
-};
+}
 
-describe('websocket server', function () {
+function load(howManyUsers, timeout) {
 
-	timeoutAfterSixtySeconds(this);
+	describe(`websocket server - ${howManyUsers} connections - ${timeout} ms timeout`, function () {
 
-	const howManyUsers = 1500;
-	const timeout = 2000;
+		timeoutAfterSixtySeconds(this);
 
-	it(`should handle at least ${howManyUsers} concurrent connections`, function () {
+		it(`concurrent connections`, function () {
 
-		const connectedSockets = [...Array(howManyUsers).keys()].map(() => {
-			return new promise((resolve, reject) => {
-				sockets.post()
-					.on('open', resolve)
-					.on('error', reject);
-			});
-		});
-
-		return promise.all(connectedSockets);
-	});
-
-	it(`should deliver messages to at least ${howManyUsers} connected users in at least ${timeout} ms`, function () {
-
-		const messages = [];
-
-		const connectedSockets = [...Array(howManyUsers).keys()].map(() => {
-			return new promise((resolve, reject) => {
-				const spy = sinon.spy();
-				sockets.post()
-					.on('open', resolve)
-					.on('error', reject)
-					.on('message', spy);
-				messages.push(spy);
-			});
-		});
-
-		return promise
-			.all(connectedSockets)
-			.then(() => {
-				sockets.post().on('open', function () {
-					this.send('ping');
+			const connectedSockets = [...Array(howManyUsers).keys()].map(() => {
+				return new promise((resolve, reject) => {
+					sockets.post()
+						.on('open', resolve)
+						.on('error', reject);
 				});
-			})
-			.delay(timeout)
-			.then(() => {
-				expect(messages.filter(message => message.called).length).to.be.equal(messages.length);
 			});
+
+			return promise.all(connectedSockets);
+		});
+
+		it(`message delivery`, function () {
+
+			const messages = [];
+
+			const connectedSockets = [...Array(howManyUsers).keys()].map(() => {
+				return new promise((resolve, reject) => {
+					const spy = sinon.spy();
+					sockets.post()
+						.on('open', resolve)
+						.on('error', reject)
+						.on('message', spy);
+					messages.push(spy);
+				});
+			});
+
+			return promise
+				.all(connectedSockets)
+				.then(() => {
+					sockets.post().on('open', function () {
+						this.send('ping');
+					});
+				})
+				.delay(timeout)
+				.then(() => {
+					expect(messages.filter(message => message.called).length).to.be.equal(messages.length);
+				});
+		});
 	});
-});
+}
